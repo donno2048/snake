145a3e33e84963653aead4c4ed724e0c06b1bab5
Elisha Hollander <just4now666666@gmail.com>
2021-03-05 12:18:25 +0200

    No need for sudo

--- snake.bash
+++ snake.bash
@@ -1,4 +1,4 @@
 sudo apt update
 sudo apt install dosbox nasm -y
 nasm snake.s -o snake.com -f bin
-sudo bash /usr/bin/dosbox -c "mount c ." -c "c:" -c "snake"
+dosbox -c "mount c ." -c "c:" -c "snake"
