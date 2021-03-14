a2d31a14465f32ce61e0a892e0d449236c946f57
Elisha Hollander <just4now666666@gmail.com>
2021-03-14 20:39:27 +0200

    Update snake.bash

--- snake.bash
+++ snake.bash
@@ -1,4 +1,1 @@
-sudo apt update
-sudo apt install dosbox nasm -y
-nasm snake.asm -o snake.com -f bin
-dosbox -c "cycles 300" -c "mount c ." -c "c:" -c "cls" -c "snake"
+sudo apt update && sudo apt install dosbox nasm -y && nasm snake.asm -o snake.com -f bin && dosbox -c "cycles 300" -c "mount c ." -c "c:" -c "cls" -c "snake"
