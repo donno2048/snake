313e41c7c7f9b16ebd16e1ec3aa0b81457c67cd5
donno2048 <just4now666666@gmail.com>
2021-10-17 15:30:21 +0300

    smaller snake

--- snake.asm
+++ snake.asm
@@ -1,288 +1,141 @@
-		org 100h
-	start:
-		mov ax, 0
-		mov word [score], ax
-		mov byte [is_game_over], al
-		mov al, 8
-		mov byte [snake_direction], al
-		mov al, 40
-		mov byte [snake_head_x], al
-		mov byte [snake_head_previous_x], al
-		mov byte [snake_tail_previous_x], al
-		mov byte [snake_tail_x], al
-		mov al, 15
-		mov byte [snake_head_y], al
-		mov byte [snake_head_previous_y], al
-		mov byte [snake_tail_y], al
-		mov byte [snake_tail_previous_y], al
-		mov bx, 0
-	.next:	
-		mov byte [buffer + bx], ' '
-		inc bx
-		cmp bx, 2000
-		jnz .next
-		mov di, 0
-	.next_x:
-		mov byte [buffer + di], 255
-		mov byte [buffer + 80 + di], 196
-		mov byte [buffer + 1920 + di], 196
-		inc di
-		cmp di, 80
-		jnz .next_x
-		mov di, 0
-	.next_y:
-		mov byte [buffer + 80 + di], 179
-		mov byte [buffer + 159 + di], 179
-		add di,80
-		cmp di, 2000
-		jnz .next_y
-	.corners:
-		mov byte [buffer + 80], 218
-		mov byte [buffer + 159], 191
-		mov byte [buffer + 1920], 192
-		mov byte [buffer + 1999], 217
-		call create_food
-	.main_loop:
-		mov si, 2
-		call update_snake_direction
-		mov al, [snake_head_y]
-		mov byte [snake_head_previous_y], al
-		mov al, [snake_head_x]
-		mov byte [snake_head_previous_x], al
-		mov ah, [snake_direction]
-		cmp ah, 8
-		jz .up
-		cmp ah, 4
-		jz .down
-		cmp ah, 2
-		jz .left
-		cmp ah, 1
-		jz .right
-	.up:
-		dec word [snake_head_y]
-		jmp .end
-	.down:
-		inc word [snake_head_y]
-		jmp .end
-	.left:
-		dec word [snake_head_x]
-		jmp .end
-	.right:
-		inc word [snake_head_x]
-	.end:
-		mov bl, [snake_direction]
-		mov ch, 0
-		mov cl, [snake_head_previous_x]
-		mov dl, [snake_head_previous_y]
-		call buffer_write
-		call check_snake_new_position
-		call print_score
-		call buffer_render
-		mov al, [is_game_over]
-		cmp al, 0
-		jz .main_loop
-		jmp start
-	exit_process:
-			mov ah, 4ch
-			int 21h
-			ret
-	buffer_write:
-		mov di, buffer
-		mov al, 80
-		mul dl
-		add ax, cx
-		add di, ax
-		mov byte [di], bl
-		ret
-	buffer_read:
-		mov di, buffer
-		mov al, 80
-		mul dl
-		add ax, cx
-		add di, ax
-		mov bl, [di]
-		ret
-	buffer_print_string:
-		.next:
-			mov al, [si]
-			cmp al, 0
-			jz .end
-			inc si
-			jmp .next
-		.end:
-			ret
-	buffer_render:
-			mov ax, 0b800h
-			mov es, ax
-			mov di, buffer
-			mov si, 0
-		.next:
-			mov bl, [di]
-			cmp bl, 8
-			jz .is_snake
-			cmp bl, 4
-			jz .is_snake
-			cmp bl, 2
-			jz .is_snake
-			cmp bl, 1
-			jz .is_snake
-			jmp .write
-		.is_snake:
-			mov bl, 219
-		.write:
-			mov byte [es:si], bl
-			inc di
-			add si, 2
-			cmp si, 4000
-			jnz .next
-			ret
-	print_score:
-			mov di, 0
-			call buffer_print_string
-			mov ax, [score]
-			mov di, 13
-		.next_digit:
-			xor dx, dx
-			mov bx, 10
-			div bx
-			push ax
-			mov al, dl
-			add al, 48
-			mov byte [buffer + di], al
-			pop ax
-			dec di
-			cmp ax, 0
-			jnz .next_digit
-			ret
-	update_snake_direction:
-			mov ah, 1
-			int 16h
-			jz .end
-			mov ah, 0h
-			int 16h
-			cmp ah, 48h
-			jz .up
-			cmp ah, 50h
-			jz .down
-			cmp ah, 4bh
-			jz .left
-			cmp ah, 4dh
-			jz .right
-			jmp update_snake_direction
-		.up:
-			mov byte [snake_direction], 8
-			jmp update_snake_direction
-		.down:
-			mov byte [snake_direction], 4
-			jmp update_snake_direction
-		.left:
-			mov byte [snake_direction], 2
-			jmp update_snake_direction
-		.right:
-			mov byte [snake_direction], 1
-			jmp update_snake_direction
-		.end:
-			ret
-	check_snake_new_position:
-			mov ch, 0
-			mov cl, [snake_head_x]
-			mov dh, 0
-			mov dl, [snake_head_y]
-			call buffer_read
-			cmp bl, 8
-			jle .set_game_over
-			cmp bl, '*'
-			je .food
-			cmp bl, ' '
-			je .empty_space
-		.set_game_over:
-			cmp al, 1
-			mov byte [is_game_over], al 
-		.write_new_head:
-			mov bl, 1
-			mov ch, 0
-			mov cl, [snake_head_x]
-			mov ch, 0
-			mov dl, [snake_head_y]
-			call buffer_write
-			ret
-		.food:
-			inc dword [score]
-			call .write_new_head
-			call create_food
-			jmp .end
-		.empty_space:
-			call update_snake_tail
-			call .write_new_head
-		.end:
-			ret
-	update_snake_tail:
-			mov al, [snake_tail_y]
-			mov byte [snake_tail_previous_y], al
-			mov al, [snake_tail_x]
-			mov byte [snake_tail_previous_x], al
-			mov ch, 0
-			mov cl, [snake_tail_x]
-			mov dh, 0
-			mov dl, [snake_tail_y]
-			call buffer_read
-			cmp bl, 8
-			jz .up
-			cmp bl, 4
-			jz .down
-			cmp bl, 2
-			jz .left
-			cmp bl, 1
-			jz .right
-			jmp exit_process
-		.up:
-			dec word [snake_tail_y]
-			jmp .end
-		.down:
-			inc word [snake_tail_y]
-			jmp .end
-		.left:
-			dec word [snake_tail_x]
-			jmp .end
-		.right:
-			inc word [snake_tail_x]
-		.end:
-			mov bl, ' '
-			mov ch, 0
-			mov cl, [snake_tail_previous_x]
-			mov ch, 0
-			mov dl, [snake_tail_previous_y]
-			call buffer_write
-		ret
-	create_food:
-		.try_again:
-			mov ah, 0
-			int 1ah
-			mov ax, dx
-			and ax, 0fffh
-			mul dx
-			mov dx, ax
-			mov ax, dx
-			mov cx, 2000
-			xor dx, dx
-			div cx
-			mov bx, dx
-			mov di, buffer
-			mov al, [di + bx]
-			cmp al, ' '
-			jnz .try_again
-			mov byte [di + bx], '*'
-			ret
+org 0x7C00
+start:
+	mov ds, ax
+	mov ss, ax
+	mov ax, 0xB800
+	mov es, ax
+	xor di, di
+	mov cx, 0x7D0
+	mov ax, 0xE20
+	pusha
+	rep stosw
+	mov ax, 0x2FE
+	mov cx, 0x26
+	mov di, 0x2A8
+	mov ah, 0x77
+	rep stosw
+	mov cx, 0x11
+.draw_block:
+	stosw
+	pusha
+	mov cx, 0x29
+	xor ah, ah
+	rep stosw
+	mov ah, 0x77
+	stosw
+	popa
+	add di, 0x9E
+	loop .draw_block
+	mov cx, 0x26
+	mov di, 0xD4A
+	rep stosw
+	popa
+	mov di, cx
+	mov bp, 0x6
+	call print_food
+.input:
+	in al, 0x60
+	and al, 0x7F
+	cmp al, 0x48
+	je .up
+	cmp al, 0x4B
+	je .left
+	cmp al, 0x4D
+	je .right
+	cmp al, 0x50
+	jne .input
+.down:
+	add di, 0xA0
+	jmp .move
+.right:
+	add di, 0x4
+	jmp .move
+.left:
+	sub di, 0x4
+	jmp .move
+.up:
+	sub di, 0xA0
+.move:
+	mov al, 0x9
+	cmp byte [es:di], 0x7
+	sete ah
+	je .alive
+	cmp byte [es:di], ' '
+	jne start
+.alive:
+	stosb
+	dec di
+	pusha
+	push es
+	push ds
+	pop es
+	mov cx, bp
+	inc cx
+	mov si, snake
+	add si, bp
+	mov di, si
+	inc di
+	inc di
+	std
+	rep movsb
+	cld
+	pop es
+	popa
+	push di
+	mov [snake], di
+	cmp ah, 0x1
+	je .food
+	mov di, [snake+bp]
+	mov al, ' '
+	stosb
+	jmp .done
+.food:
+	inc bp
+	inc bp
+	mov di, 0x230
+	add word [score], 0x1
+	mov ax, [score]
+	mov bl, 0xA
+.print_score:
+	div bl
+	xchg al, ah
+	add al, '0'
+	stosb
+	sub di, 0x3
+	mov al, ah
+	xor ah, ah
+	or al, al
+	jnz .print_score
+	call print_food
+.done:
+	pop di
+	jmp .input
+print_food:
+	pusha
+.rand:
+	mov di, 0xAB ; FIXME: not random
+	; cannot use clock cycles because it's 1 so it doesn't change dramatically
+	; cannot use drivers because there aren't any
+	; cannot use rdrand, rdseed etc. because it's too slow and we're on 1 cycle
+	; cannot use garbage memory because it's all set to zero on dosbox
+	cmp di, 0x0 ; overflow protection - we won't remove the add on line 133 even though we use that because then the mod calculation will be too long
+	jl .rand
+	cmp di, 0x280
+	jge .rand
+	mov dx, di
+	add dx, 0x28
+.mod:
+	sub dx, 0x28
+	cmp dx, 0x28
+	jge .mod
+	cmp dx, 0x12
+	jge .rand
+	add di, 0xD3
+	shl di, 0x2
+	mov al, 0x7
+	stosb
+	popa
+	ret
 section .bss
-		score resw 1
-		is_game_over resb 1
-		snake_direction resb 1
-		snake_head_x resb 1
-		snake_head_y resb 1
-		snake_head_previous_x resb 1
-		snake_head_previous_y resb 1
-		snake_tail_x resb 1
-		snake_tail_y resb 1
-		snake_tail_previous_x resb 1
-		snake_tail_previous_y resb 1
-		buffer resb 2000
+score: resd 1
+snake:
--- snake.bash
+++
@@ -1,1 +0,0 @@
-sudo apt update && sudo apt install dosbox nasm -y && nasm snake.asm -o snake.com -f bin && dosbox -c "cycles 300" -c "mount c ." -c "c:" -c "cls" -c "snake"
