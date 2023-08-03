e4b708809dace17acbcbd5135efc1db1e4d6157c
dl-tg <90642859+dl-tg@users.noreply.github.com>
2023-08-04 00:36:28 +0300

    Update snake.asm

--- snake.asm
+++ snake.asm
@@ -1,43 +1,42 @@
-push 0xB800
+push 0xb800
 pop ds
-mov cx, 0xFA0
+mov cx, 0xfa0
 start:
-	mov ax, 0x3
-	int 0x10
-	mov di, 0x7D0
-	lea si, [bp-0x4]
+mov ax, 0x3
+int 0x10
+mov di, 0x7d0
+lea si, [bp-4]
 .food:
-	imul bx, bp
-	and bx, cx
-	cmp [bx], ch
-	je .food
-	mov [bx], cl
+imul bx, bp
+and bx, cx
+cmp [bx], ch
+je .food
+mov [bx], cl
 .input:
-	in al, 0x60
-	mov bx, 0x4
-	and al, 0x1E
-	jp $+0x4
-	mov bl, cl
-	and al, 0x14
-	jz $+0x4
-	neg bx
-	sub di, bx
-	cmp di, cx
-	ja start
-	sar bx, 0x1
-	lea ax, [di+bx+0x2]
-	div cl
-	and ah, ah
-	jz start
-	cmp [di], ch
-	je start
-	mov [bp], di
-	inc bp
-	inc bp
-	cmp [di], cl
-	mov [di], ch
-	je .food
-	es lodsw
-	xchg ax, bx
-	mov [bx], ah
-	jmp SHORT .input
+in al, 0x60
+mov bx, 0x4
+and al, 0x1e
+jp $+4
+mov bl, cl
+and al, 0x14
+jz $+4
+neg bx
+sub di, bx
+cmp di, cx
+ja start
+sar bx, 1
+lea ax, [di+bx+2]
+div cl
+and ah, ah
+jz start
+cmp [di], ch
+je start
+mov [bp], di
+add bp, 2
+cmp [di], cl
+mov [di], ch
+je .food
+es lodsw
+xchg ax, bx
+mov [bx], ah
+jmp .input
