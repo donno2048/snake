154312f50a422a7122e51fa9b15f3aa7b6b5019a
dl-tg <90642859+dl-tg@users.noreply.github.com>
2023-08-07 15:40:37 +0300

    Formatting and more clear comments
    IMO, the comments provided in original commit were too unclear for people who are not-so good in Assembly. So now they're much more clear, and I'm sure that people who know x86 Assembly will already understand it. However, I'm open to discussion about that change.

--- snake.asm
+++ snake.asm
@@ -1,42 +1,48 @@
-push 0xB800                 ; push the offset to video memory for DMA
-pop ds                      ; pop that value into the data segment register so that every memory access will edit the video memory
-mov cx, 0xFA0               ; move the size of the screen in text mode 3 into cx, also the cp437 values of the food and snake body into ch, cl, and the width of the screen to cl
-std                         ; set direction flag to inverse the direction of lodsw for reading the stack as FIFO
-start:                      ; start new game setup
-	mov ax, 0x3         ; set ah=0 (change video mode) and al=3 (80x25 16 color text mode)
-	int 0x10            ; video BIOS interrupt
-	mov di, 0x7D0       ; move the middle of the screen (0xFA0/2) into di which is the initial position for the snake
-	mov si, sp          ; make si point to the stack by moving the value of the stack pointer into it so that every lods will access the stack
-.food:                      ; print new food
-	imul bx, sp         ; multiply bx by sp to pseudo randomize bx which points to the place we'll put the food
-	and bx, cx          ; and bx with cx to make it divisible by 4 (so it'll be eatable by the snake) and fit on the screen
-	cmp [bx], ch        ; check if bx points to a place on the screen with a snake body character 
-	je .food            ; if it is try to get another random bx
-	mov [bx], cl        ; else put a food character there
-.input:                     ; process keyboard input
-	in al, 0x60         ; scan port 60h (keyboard input)
-	mov bx, 0x4         ; set the default distance to move the snake to 4 (one step leftwards)
-	and al, 0x1E        ; and al with 1e, 48 and 50 (down and up) will yield a parity flag of 0 and 4b and 4d (sides) of 1
-	jp $+0x4            ; if the input is one of the sides jump over the next instruction
-	mov bl, cl          ; set the value of the move to cl with is the width of the screen (i.e. one step up)
-	and al, 0x14        ; and al with 14h, (48 & 1e) & 14h = (4b & 1e) & 14h = 0 and (50 & 1e) & 14h ≠ 0 and (4d & 1e) & 14h ≠ 0
-	jz $+0x4            ; if the result was zero jump over the next instruction 
-	neg bx              ; negate bx resulting in inversing the direction of the move
-	sub di, bx          ; subtract bx from di which points to the next position of the snake's head, essentially executing the move
-	cmp di, cx          ; subtract cx from di without saving the result
-	ja start            ; if the result of the previous operation is above zero (unsigned so if di>cx or di<0 which means the snake went through a wall) restart the game
-	sar bx, 0x1         ; divide bx by two so in the next operation after bx+2 [4 -> 4, -4 -> 0, ±160 -> something indivisible by 4]
-	lea ax, [di+bx+0x2] ; di+bx+2 will find the minimum of previous di and current di plus 4 for horizontal movement and something not divisible by 4 for vertical
-	div cl              ; for horizontal movement divide by the width of the screen to check if we moved a row, irrelevant for vertical movement as it's divisible by 4
-	and ah, ah          ; and the remainder with itself to check if it's zero 
-	jz start            ; if the result of the previous operation was zero (the snake hit a side wall) reset
-	cmp [di], ch        ; compare the character at the new position of the snake to the snake character 
-	je start            ; if the result of the previous comparison yielded an equal response reset the game (the snake hit itself)
-	push di             ; push di to the stack to save the new position 
-	cmp [di], cl        ; compare the character at the new position of the snake to the food character 
-	mov [di], ch        ; move the snake character to the new position 
-	je .food            ; if the result of the last comparison yielded an equal response (the snake ate the food) print new food and don't continue to delete the snake's tail
-	es lodsw            ; use the fact si points to the stack to load the first  thing pushed to the stack (the end of the tail) and "pop" it into ax
-	xchg ax, bx         ; exchange ax and bx to save one byte and to put the position in ax in bx and to move bh (which is invisible in cp437) into ah
-	mov [bx], ah        ; move the invisible character instead of the snake character 
-	jmp SHORT .input    ; loop back to process keyboard input
+push 0xB800                 ; Push the offset to video memory for DMA (Direct Memory Access)
+pop ds                      ; Load the value into the data segment register to access video memory
+mov cx, 0xFA0               ; Store the size of the screen in text mode 3 into cx (80x25 characters), also store cp437 values for food and snake body, and the screen width in cl
+std                         ; Set the direction flag to reverse for FIFO processing
+
+start:                      ; Start new game setup
+    mov ax, 0x3             ; Set ah=0 (change video mode) and al=3 (80x25 16 color text mode)
+    int 0x10                ; Trigger Video BIOS interrupt
+    mov di, 0x7D0           ; Set the middle of the screen (0xFA0/2) into di as the initial snake position
+    mov si, sp              ; Point si to the stack (using the stack pointer) to access user input
+
+.food:                      ; Place new food
+    imul bx, sp             ; Multiply bx by sp to pseudo-randomize bx, which points to the food position
+    and bx, cx              ; Make bx divisible by 4 to fit on the screen (align with character positions)
+    cmp [bx], ch            ; Check if bx points to a position with a snake body character
+    je .food                ; If it does, find another random bx
+    mov [bx], cl            ; Otherwise, place a food character at that position
+
+.input:                     ; Process keyboard input
+    in al, 0x60             ; Read from keyboard input port
+    mov bx, 0x4             ; Set default distance for snake movement to 4 (leftwards)
+    and al, 0x1E            ; Extract arrow key inputs (up, down, left, right)
+    jp $+0x4                ; Skip next instruction if input is an arrow key
+    mov bl, cl              ; Use width of screen (cl) as distance for horizontal movement
+    and al, 0x14            ; Extract only left and right arrow key inputs
+    jz $+0x4                ; Skip next instruction if input is left or right
+    neg bx                  ; Invert direction for left or right movement
+    sub di, bx              ; Update snake's head position based on movement
+
+    cmp di, cx              ; Check if snake's head position is beyond screen boundaries
+    ja start                ; If out of bounds, restart the game
+    sar bx, 0x1             ; Divide bx by 2 to prepare for next calculation
+    lea ax, [di+bx+0x2]     ; Calculate new position after move (considering screen boundaries)
+    div cl                  ; Divide by screen width (cl) to check if a row was crossed
+    and ah, ah              ; Check if remainder is zero (horizontal movement check)
+    jz start                ; If it is, reset the game
+
+    cmp [di], ch            ; Compare character at new snake position with snake body character
+    je start                ; If equal, reset the game (snake hits itself)
+    push di                 ; Save new snake head position on the stack
+    cmp [di], cl            ; Compare character at new position with food character
+    mov [di], ch            ; Move the snake body character to the new position
+    je .food                ; If it's food, place new food and skip tail removal
+
+    es lodsw                ; Load the last position (tail) from the stack and pop it into ax
+    xchg ax, bx             ; Exchange ax and bx to prepare for tail removal
+    mov [bx], ah            ; Move an invisible character instead of the snake body
+    jmp SHORT .input        ; Loop back to process more keyboard input
