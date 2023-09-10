cafdde4289f4811ff11359c66ee28f34c3160530
donno2048 <just4now666666@gmail.com>
2023-09-10 16:49:23 +0300

    better html and js

--- main.js
+++ main.js
@@ -1,29 +1,26 @@
-Dos(document.getElementById("jsdos"), { cycles: 1, autolock: true, onprogress: (stage, total, loaded) => {document.getElementById("Bar").style.width = Math.floor(loaded * 99 / total) + "%"}}).ready((fs, main) => {
-    fs.extract("snake.zip").then(() => {
-        document.getElementById("Bar").style.height = "0px";
-        main(["-c", "snake"]).then((ci) => {swipedetect(document.getElementById("jsdos"), (swipedir) => {if (swipedir) ci.simulateKeyPress(36 + swipedir);})})
-    });
-});
-function swipedetect(el, callback) {
-    var touchsurface = el, swipedir, startX, startY, distX, distY, threshold = 100, restraint = 90, allowedTime = 1000, elapsedTime, startTime;
-    touchsurface.addEventListener("touchstart", function(e){
-        var touchobj = e.changedTouches[0]
-        swipedir = 0
-        dist = 0
-        startX = touchobj.pageX
-        startY = touchobj.pageY
-        startTime = new Date().getTime()
-    }, false)
-    touchsurface.addEventListener("touchmove", function(e){}, false)
-    touchsurface.addEventListener("touchend", function(e){
-        var touchobj = e.changedTouches[0]
-        distX = touchobj.pageX - startX
-        distY = touchobj.pageY - startY
-        elapsedTime = new Date().getTime() - startTime
-        if (elapsedTime <= allowedTime){
-            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){swipedir = (distX < 0)? 1 : 3}
-            else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){swipedir = (distY < 0)? 2 : 4}
-        }
-        callback(swipedir)
-    }, false)
+const canvas = document.getElementById("jsdos");
+
+Dos(canvas, { cycles: 1, onprogress: ()=>{} }).ready((fs, main) => 
+    fs.extract("snake.zip").then(() =>
+        main(["snake.com"]).then((ci) => 
+            swipedetect((swipedir) => swipedir && ci.simulateKeyPress(36 + swipedir))
+        )
+    )
+);
+
+function swipedetect(callback) {
+    var startX, startY;
+
+    canvas.addEventListener("touchstart", function(e) {
+        startX = e.changedTouches[0].pageX;
+        startY = e.changedTouches[0].pageY;
+    }, false);
+
+    canvas.addEventListener("touchend", function(e) {
+        distX = e.changedTouches[0].pageX - startX;
+        distY = e.changedTouches[0].pageY - startY;
+        if (Math.abs(distX) >= 100 && Math.abs(distY) <= 100) swipedir = (distX < 0) ? 1 : 3;
+        else if (Math.abs(distY) >= 100 && Math.abs(distX) <= 100) swipedir = (distY < 0) ? 2 : 4;
+        callback(swipedir);
+    }, false);
 }
