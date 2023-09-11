54414c2772a491e260a18b7486931ec4d7e92746
Elisha Hollander <just4now666666@gmail.com>
2023-09-11 17:48:49 +0300

    use better formatting

--- docs/update.py
+++ docs/update.py
@@ -8,4 +8,4 @@ for div in range(int(length ** .5), 0, -1):
     if not length % div:
         break
 
-open("../README.md", "w").write(open('template.md').read() % (length // 2, "\n".join(findall('.' * (length // div), hexdata))))
+open("../README.md", "w").write(open('template.md').read().format(size = length // 2, hex = "\n".join(findall('.' * (length // div), hexdata))))
