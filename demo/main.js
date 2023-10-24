253ead02357b264951458a6ac88e0e32f2411096
Elisha Hollander <just4now666666@gmail.com>
2023-10-24 09:08:25 +0300

    on mobile add touch delay

--- demo/main.js
+++ demo/main.js
@@ -1,10 +1,11 @@
 const canvas = document.getElementById("jsdos");
 const speed = document.getElementById("speed");
+let delayId;
 
 Dos(canvas, { cycles: 1, onprogress: ()=>{} }).ready((fs, main) =>
     fs.extract("snake.zip").then(() =>
         main(["snake.com"]).then(ci => {
-            runLag();
+            delayId = setInterval(runLag, 1);
             swipedetect(swipedir => swipedir && ci.simulateKeyPress(36 + swipedir));
         })
     )
@@ -16,19 +17,22 @@ function swipedetect(callback) {
     canvas.addEventListener("touchstart", function(e) {
         startX = e.changedTouches[0].pageX;
         startY = e.changedTouches[0].pageY;
+        clearInterval(delayId);
+        delayId = setInterval(runLag, 1, 5);
     }, false);
 
     canvas.addEventListener("touchend", function(e) {
+        clearInterval(delayId);
         distX = e.changedTouches[0].pageX - startX;
         distY = e.changedTouches[0].pageY - startY;
-        if (Math.abs(distX) >= 50 && Math.abs(distY) <= 50) swipedir = (distX < 0) ? 1 : 3;
-        else if (Math.abs(distY) >= 50 && Math.abs(distX) <= 50) swipedir = (distY < 0) ? 2 : 4;
+        if (Math.abs(distX) >= 100) swipedir = (distX < 0) ? 1 : 3;
+        else if (Math.abs(distY) >= 100) swipedir = (distY < 0) ? 2 : 4;
         callback(swipedir);
+        delayId = setInterval(runLag, 1);
     }, false);
 }
 
-function runLag() {
-    var start = Date.now();
-    while (Date.now() < start + 100 - speed.value);
-    setTimeout(runLag, .1);
+function runLag(factor = 1) {
+    const start = Date.now();
+    while (Date.now() < start + (100 - speed.value) * factor);
 }
