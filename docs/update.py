0719f2c474ec43442da77041159e127d66273382
Elisha Hollander <just4now666666@gmail.com>
2024-01-03 22:00:14 +0200

    search above sqrt

--- docs/update.py
+++ docs/update.py
@@ -5,8 +5,6 @@ hexdata = open(0).read().replace("\n", "")
 
 length = len(hexdata)
 
-for div in range(int(length ** .5), 0, -1):
-    if not length % div:
-        break
+div = next(filter(lambda x: not length % x, range(int(length ** .5), length // 2 + 1)))
 
-open("README.md", "w").write(open(Path(__file__).parent.resolve() / 'template.md').read().format(size = length // 2, hex = "\n".join(findall('.' * (length // div), hexdata))))
+open("README.md", "w").write(open(Path(__file__).parent.resolve() / 'template.md').read().format(size = length // 2, hex = "\n".join(findall('.' * div, hexdata))))
