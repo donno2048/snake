745ca119ab45e8ae3de040e1240e4df7fb173a6e
donno2048 <just4now666666@gmail.com>
2024-01-18 14:32:27 +0000

    use python instead of lighttpd

--- main.sh
+++ main.sh
@@ -2,6 +2,4 @@
 set -e
 nasm -f bin snake.asm -o snake.com
 zip demo/snake.zip snake.com
-config=$(mktemp)
-sed "s/PWD/${PWD//\//\\\/}/" lighttpd.conf > "$config"
-lighttpd -D -f "$config" || rm "$config"
+python3 -m http.server -d demo
