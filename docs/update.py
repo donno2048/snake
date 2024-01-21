2c093b884a1f432729e1a0c6dcdf9c67bbd8d9f1
Elisha Hollander <just4now666666@gmail.com>
2024-01-21 19:06:20 +0200

    align by bytes not hex

--- docs/update.py
+++ docs/update.py
@@ -3,8 +3,8 @@ from re import findall
 
 hexdata = open(0).read().replace("\n", "")
 
-length = len(hexdata)
+length = len(hexdata) // 2
 
-div = next(filter(lambda x: not length % x, range(int(length ** .5), length // 2 + 1)))
+div = next(filter(lambda x: not length % x, range(int(length ** .5), length)))
 
-open("README.md", "w").write(open(Path(__file__).parent.resolve() / 'template.md').read().format(size = length // 2, hex = "\n".join(findall('.' * div, hexdata))))
+open("README.md", "w").write(open(Path(__file__).parent.resolve() / 'template.md').read().format(size = length, hex = "\n".join(findall('..' * div, hexdata))))
