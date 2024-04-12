285f63602dd0306297fb8055ac0c88d2cd727337
Elisha Hollander <just4now666666@gmail.com>
2024-04-12 09:47:03 +0300

    hex count must be even

--- docs/update.py
+++ docs/update.py
@@ -6,6 +6,6 @@ hexdata = open(0).read().replace("\n", "")
 
 length = len(hexdata)
 
-div = next(filter(lambda x: not length % x, range(int(length ** .5), length + 1)))
+div = next(filter(lambda x: not length % x, range(int(length ** .5), length)))
 
 open("README.md", "w").write(open(Path(__file__).parent.resolve() / 'template.md').read().format(size = length // 2, hex = "\n".join(findall('.' * div, hexdata)), platform = get_platform(), empty_size = Path("a.out").stat().st_size))
