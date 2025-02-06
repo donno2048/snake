ccacd1b2e5b0a29347a0c510d524228ec1ca33ea
Elisha Hollander <just4now666666@gmail.com>
2025-02-06 10:04:56 +0200

    refactor update.py

--- docs/update.py
+++ docs/update.py
@@ -1,11 +1,17 @@
+from sys import stdin
 from re import findall
 from pathlib import Path
 from sysconfig import get_platform
 
-hexdata = open(0).read().replace("\n", "")
+hexdata = stdin.read().replace("\n", "")
 
 length = len(hexdata)
 
-div = next(filter(lambda x: not length % x, range(int(length ** .5), length)))
+div = next(filter(lambda x: length % x == 0, range(int(length ** .5), length)))
 
-open("README.md", "w").write(open(Path(__file__).parent.resolve() / 'template.md').read().format(size = length // 2, hex = "\n".join(findall('.' * div, hexdata)), platform = get_platform(), empty_size = Path("a.out").stat().st_size))
+open("README.md", "w").write(open("docs/template.md").read().format(
+  size = length // 2,
+  hex = "\n".join(findall('.' * div, hexdata)),
+  platform = get_platform(),
+  empty_size = Path("a.out").stat().st_size
+))
