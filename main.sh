b608977bbd95aec3c6f8a0ad578b5ba1cc743f9c
Elisha Hollander <just4now666666@gmail.com>
2025-02-20 17:45:15 +0000

    better `main.sh`

--- main.sh
+++ main.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-
-{ command -v nasm && command -v python3; } > /dev/null || \
-    { echo "Error: nasm or python3 is not installed.";
-      echo "Try 'apt install nasm python3'."; exit 1; }
+if ! command -v nasm >/dev/null; then
+    echo "Error: 'nasm' is not installed. Try 'apt install nasm'."
+    exit 1
+fi
+if ! command -v python3 >/dev/null; then
+    echo "Error: 'python3' is not installed. Try 'apt install python3'."
+    exit 1
+fi
 nasm -f bin snake.asm -o demo/snake.com || exit 1
 python3 -m http.server -d demo
